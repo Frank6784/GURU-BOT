732d96d0ebbfcd3c0a6d813173a17ffc002dc22c
Frank6784 <156773585+Frank6784@users.noreply.github.com>
2024-01-16 20:43:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254743439840', 'FRANK', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
